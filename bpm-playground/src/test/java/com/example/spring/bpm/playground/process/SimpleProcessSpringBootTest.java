290efeec4fb9381de215ce57aa6f98e4e9b1dfe3
João Luzio <joao.luzio@gmail.com>
2023-11-23 14:14:03 +0000

    Examples

--- bpm-playground/src/test/java/com/example/spring/bpm/playground/process/SimpleProcessSpringBootTest.java
+++ bpm-playground/src/test/java/com/example/spring/bpm/playground/process/SimpleProcessSpringBootTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "process-bootstrap=false")
+@SpringBootTest
 @Slf4j
 @Tag(TestTags.NON_ISOLATED_TEST)
 class SimpleProcessSpringBootTest {
